5da8cf684f93b2d77c27bb43894701060f8f025b
showeye <tadeusz.jargilo@gmail.com>
2015-04-12 17:09:14 +0200

    ready to go 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,4 @@
-## I have changed sth - see: HAHAAH
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function that creates list with stored given matrix and inverted data
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -20,7 +16,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function that checks if inversion already exists, if not, it counts it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
